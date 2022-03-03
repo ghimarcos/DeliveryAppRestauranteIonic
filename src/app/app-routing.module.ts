46120a97790d130eb9f9cc9f8f2ae84c98687dd4
Giovani Ribeiro <ghimarcos123l@gmail.com>
2022-03-02 21:26:47 -0300

    Criado Menu de Cardápio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,45 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    loadChildren: () =>
+      import('./folder/folder.module').then((m) => m.FolderPageModule),
+  },
+  {
+    path: 'cardapio',
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./cardapio/cardapio-list/cardapio-list.module').then(
+            (m) => m.CardapioListPageModule
+          ),
+      },
+      {
+        path: 'new',
+        loadChildren: () =>
+          import('./cardapio/cardapio-edit/cardapio-edit.module').then(
+            (m) => m.CardapioEditPageModule
+          ),
+      },
+      {
+        path: 'edit/:id',
+        loadChildren: () =>
+          import('./cardapio/cardapio-edit/cardapio-edit.module').then(
+            (m) => m.CardapioEditPageModule
+          ),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
